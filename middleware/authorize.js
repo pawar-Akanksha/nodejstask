879d326bdb5bbe41a23d0ace6fa488622072274d
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-07-23 03:45:53 -0700

    added changes

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,3 +1,44 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Authorization
+ *   description: User authorization middleware
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * /api/authorize:
+ *   get:
+ *     summary: Test authorization middleware
+ *     tags: [Authorization]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: role
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Role to check authorization against
+ *     responses:
+ *       200:
+ *         description: Authorization successful
+ *       401:
+ *         description: Unauthorized - No token provided or invalid token
+ *       403:
+ *         description: Forbidden - User role not authorized
+ */
+
 const authorize = (roles) => {
   console.log("incoming=>",roles)
     return (req, res, next) => {
