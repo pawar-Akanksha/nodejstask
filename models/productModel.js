8c138fde5d62afde13d756e34f241a26a74b538d
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-07-23 03:48:23 -0700

    added changes

--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,1 @@
 
-const mongoose = require("mongoose");
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  category: String,
-  price: Number,
-});
-
-const Product = mongoose.model("Product", productSchema);
-
-module.exports = Product;
