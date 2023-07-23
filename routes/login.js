a196cee7aaeb4bcaa31a0c964276280f1d29b8aa
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-07-23 01:04:43 -0700

    added swagger annotations

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,10 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: User authentication endpoints
+ */
+
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
@@ -6,6 +13,47 @@ const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 router.use(express.json());
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: User's username
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: JWT token for authentication
+ *                 message:
+ *                   type: string
+ *                   description: Success message
+ *       404:
+ *         description: Incorrect username or password
+ */
+
 
 router.post('/login', async (req, res) => {
   const {username, password} = req.body;
